d2f4df1110a7ddfb3e55f236294604c5ec774cc4
Dan Luchi <dluchi@gmail.com>
2015-08-31 14:39:43 -0400

    Validate presence of event.text

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   validates :key, presence: true, uniqueness: true
+  validates :text, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,4 +3,6 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   should validate_presence_of(:key)
   should validate_uniqueness_of(:key)
+
+  should validate_presence_of(:text)
 end
